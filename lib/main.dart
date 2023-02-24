500a69b5c9cba62fb6f26bad68d4628fdeba1813
Lalli (Naiade Lima) <nainiixd@gmail.com>
2023-02-24 14:46:33 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'home_page.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -24,8 +23,55 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         brightness: Brightness.dark,
       ),
-      home: const MyHomePage(title: 'Flutter Example App'),
+      home: CircularContainers(),
       debugShowCheckedModeBanner: false,
+    );
+  }
+}
+class CircularContainers extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final double containerSize = min(size.width, size.height) / 7;
+    final double centerX = size.width / 2;
+    final double centerY = size.height / 2;
+
+    return Container(
+      height: size.height,
+      width: size.width,
+      child: Stack(
+        children: [
+          Positioned(
+            left: centerX - containerSize / 2,
+            top: centerY - containerSize / 2,
+            child: Container(
+              height: containerSize,
+              width: containerSize,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+          ...List.generate(10, (index) {
+            final angle = 2 * pi * index / 10;
+            final x = centerX + cos(angle) * 2.5 * containerSize;
+            final y = centerY + sin(angle) * 2.5 * containerSize;
+            return Positioned(
+              left: x - containerSize / 2,
+              top: y - containerSize / 2,
+              child: Container(
+                height: containerSize,
+                width: containerSize,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            );
+          }),
+        ],
+      ),
     );
   }
 }
